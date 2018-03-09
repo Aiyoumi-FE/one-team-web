37ed733fcb9dbbcd61411f6130e5d1ed7874ca63
zhaozhisheng <zhaozhisheng@aiyoumi.com>
2018-03-09 15:04:39 +0800

    update  个人设置页面更新--样式

--- src/module/user/settings/index.vue
+++ src/module/user/settings/index.vue
@@ -1,39 +1,120 @@
 <template>
-    <div>
+    <div class="page_setting">
         <h2>个人设置</h2>
         <a href="javascript:;" class="link-delete" @click="submitSignout">退出团队</a>
         <div class="setting-cells">
-            <div class="setting-item">
+            <div class="part_photo">
                 <label class="label-img" for="file">
                     <div class="setting-img"><img src="../image/cat.png" alt=""></div>
-                    <a href="javascript:;">选择新头像</a>
+                    <p class="txt_photo__title">选择新头像</p>
+                    <p class="txt_photo__subtitle">可以选择png/jpg图片作为头像</p>
                     <input class="input-file" type="file" name="file" id="file" @change="upImg($event)" enctype="multipart/form-data">
                 </label>
             </div>
-            <div class="setting-item">
-                <label>名字</label>
-                <div class="setting-item__filed">
-                    <input class="input" type="text" v-model="nickName">
-                </div>
-            </div>
-            <div class="setting-item">
-                <label>邮箱</label>
-                <div class="setting-item__filed">
-                    <input type="text" class="input">
-                    <a class="ml20" href="javascript:;">修改邮箱</a>
-                </div>
-            </div>
-            <div class="setting-item">
-                <label>密码</label>
-                <div class="setting-item__filed">
-                    <a class="ml20" href="javascript:;">修改密码</a>
-                    <a class="ml20" href="javascript:;">找回密码</a>
-                </div>
-            </div>
-            <div class="setting-item">
-                <label>&nbsp;</label>
-                <div class="btn week-btn">提交</div>
-            </div>
+            <el-collapse accordion>
+                <el-collapse-item>
+                    <template slot="title">
+                        <p class="setting-item" @click="setEditFlag('name')">
+                            <label class="setting-item__label">用&nbsp;户&nbsp;名</label>
+                            <span>{{user.name | contentFilter(editflag !== 'name', '用户名')}}</span>
+                            <a href="javascript:;" class="btn_edit">{{btnTxt | editBtnFilter(editflag !== 'name') }}</a>
+                        </p>
+                    </template>
+                    <div class="area_edit">
+                        <el-input
+                            class="edit_input disb"
+                            v-model="userEdit.name"
+                            placeholder="用户名"
+                            prefix-icon="el-icon-star-off">
+                        </el-input>
+                        <el-button class="disb" type="info" @click="saveInfo('name')">确定</el-button>
+                    </div>
+                </el-collapse-item>
+                <el-collapse-item>
+                    <template slot="title">
+                        <p class="setting-item" @click="setEditFlag('email')">
+                            <label class="setting-item__label">邮&nbsp;&nbsp;&nbsp;箱</label>
+                            <span>{{user.email | contentFilter(editflag !== 'email', '邮箱')}}</span>
+                            <a href="javascript:;" class="btn_edit">{{btnTxt | editBtnFilter(editflag !== 'email') }}</a>
+                        </p>
+                    </template>
+                    <div class="area_edit">
+                        <p>
+                            <el-input
+                                class="edit_input"
+                                v-model="userEdit.emailPwd"
+                                placeholder="验证码"
+                                prefix-icon="el-icon-star-off">
+                            </el-input>
+                            <span>发送验证码</span>
+                        </p>
+                        <el-input
+                            class="edit_input disb"
+                            v-model="userEdit.emailNew"
+                            placeholder="新邮箱"
+                            prefix-icon="el-icon-star-off">
+                        </el-input>
+                        <p>
+                            <el-input
+                                class="edit_input"
+                                v-model="userEdit.emailPwdNew"
+                                placeholder="新邮箱验证码"
+                                prefix-icon="el-icon-star-off">
+                            </el-input>
+                            <span>发送验证码</span>
+                        </p>
+                        <el-button class="disb" type="info" @click="saveInfo('email')">确定</el-button>
+                    </div>
+                </el-collapse-item>
+                <el-collapse-item>
+                    <template slot="title">
+                        <p class="setting-item" @click="setEditFlag('phone')">
+                            <label class="setting-item__label">手&nbsp;&nbsp;&nbsp;机</label>
+                            <span>{{user.phone | contentFilter(editflag !== 'phone', '手机号')}}</span>
+                            <a href="javascript:;" class="btn_edit">{{btnTxt | editBtnFilter(editflag !== 'phone') }}</a>
+                        </p>
+                    </template>
+                    <div class="area_edit">
+                        <el-input
+                            class="edit_input disb"
+                            v-model="userEdit.phone"
+                            placeholder="新手机号"
+                            prefix-icon="el-icon-star-off">
+                        </el-input>
+                        <el-button class="disb" type="info" @click="saveInfo('phone')">确定</el-button>
+                    </div>
+                </el-collapse-item>
+                <el-collapse-item>
+                    <template slot="title">
+                        <p class="setting-item" @click="setEditFlag('password')">
+                            <label class="setting-item__label">密&nbsp;&nbsp;&nbsp;码</label>
+                            <span>重置密码</span>
+                            <a href="javascript:;" class="btn_edit">{{btnTxt | editBtnFilter(editflag !== 'password') }}</a>
+                        </p>
+                    </template>
+                    <div class="area_edit">
+                        <el-input
+                            class="edit_input disb"
+                            v-model="userEdit.password"
+                            placeholder="老密码"
+                            prefix-icon="el-icon-star-off">
+                        </el-input>
+                        <el-input
+                            class="edit_input disb"
+                            v-model="userEdit.passwordNew"
+                            placeholder="新密码"
+                            prefix-icon="el-icon-star-off">
+                        </el-input>
+                        <el-input
+                            class="edit_input disb"
+                            v-model="userEdit.passwordNewConfirm"
+                            placeholder="新密码确认"
+                            prefix-icon="el-icon-star-off">
+                        </el-input>
+                        <el-button class="disb" type="info" @click="saveInfo('password')">确定</el-button>
+                    </div>
+                </el-collapse-item>
+            </el-collapse>
         </div>
     </div>
 </template>
@@ -41,12 +122,47 @@
 import {
     signout
 } from '@/store/home'
+import {
+    Collapse,
+    CollapseItem
+} from 'element-ui'
 export default {
     name: 'members-settings',
     data() {
         return {
-            nickName: ''
+            nickName: '',
+            activeNames: '',
+            editflag: '',
+            user: {
+                name: '李天霸',
+                email: 'tianba@ico.com',
+                phone: '15268175233'
+            },
+            userEdit: {
+                name: '',
+                email: '',
+                emailPwd: '',
+                emailNew: '',
+                emailPwdNew: '',
+                phone: '',
+                password: '',
+                passwordNew: '',
+                passwordNewConfirm: ''
+            },
+            btnTxt: '编辑'
         }
+    },
+    filters: {
+        contentFilter(val, flag, itemName) {
+            return !flag ? ('原' + itemName + ': ' + val) : val
+        },
+        editBtnFilter(val, flag) {
+            return !flag ? '  ' : val
+        }
+    },
+    components: {
+        'el-collapse': Collapse,
+        'el-collapse-item': CollapseItem
     },
     methods: {
         submitSignout() {
@@ -86,13 +202,62 @@ export default {
                     }
                 }
             }
+        },
+        setEditFlag(item) {
+            if (this.editflag === item) {
+                this.editflag = ''
+            } else {
+                this.editflag = item
+            }
+        },
+        saveInfo(item) {
+
         }
     }
 }
 
 </script>
 <style lang='scss' scoped>
-// @import '../../../var.scss';
+.page_setting {
+    padding: 35px;
+    .setting-cells {
+        padding: 0 10px;
+    }
+    .setting-item {
+        /*display: flex;
+        justify-content: space-between;
+        align-items: center;*/
+    }
+    .setting-item__label {
+        /*flex-basis: 15%;*/
+        display: inline-block;
+        width: 30%;
+    }
+    .area_edit {
+        padding-left: 30%;
+    }
+    .btn_edit {
+        float: right;
+    }
+    .setting-item__filed {
+        padding-left: 10px;
+        display: inline-block;
+    }
+    .edit_input {
+        max-width: 200px;
+        margin-bottom: 5px
+    }
+    /*.area_edit {
+        display: flex;
+        justify-content: space-between;
+    }*/
+    .fr {
+        float: right;
+    }
+    .disb {
+        display: block;
+    }
+}
 .link-delete {
     display: block;
     position: absolute;
@@ -107,11 +272,26 @@ h2 {
     font-size: 18px;
 }
 
-.setting-item {
+.part_photo {
+    height: 90px;
     font-size: 14px;
-    margin-bottom: 20px;
     clear: left;
-    line-height: 30px;
+    line-height: 40px;
+    position: relative;
+    /*border-bottom: 1px solid #999;*/
+    p {
+        line-height: 1.5rem;
+    }
+    .txt_photo__title {
+        font-size: 18px;
+        color: #4ea5ad;
+        padding: 14px 80px 0;
+    }
+    .txt_photo__subtitle {
+        font-size: 14px;
+        color: #ccc;
+        padding-left: 80px;
+    }
     label {
         float: left;
         width: 60px;
@@ -138,15 +318,11 @@ h2 {
         }
     }
     .setting-img {
-        width: 60px;
-        height: 60px;
+        width: 70px;
+        height: 70px;
         border-radius: 100%;
         float: left;
         overflow: hidden;
-    }
-    &__filed {
-        margin-left: 70px;
-        font-size: 12px;
     }
 }
 
