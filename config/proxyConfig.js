aed15a73f234424c4d65f285bc21e43411c927c5
dotview <dotview@163.com>
2018-07-03 18:14:27 +0800

    * update env

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     proxy: {
         '/+(api|user|team|week|common)/**': {
             // target: 'http://172.16.19.191:8081/',
-            target: 'http://localhost:8081/',
+            target: process.env.TARGET || 'http://localhost:8081/',
             changeOrigin: true, //是否跨域
             pathRewrite: {} //需要rewrite的  
         }
