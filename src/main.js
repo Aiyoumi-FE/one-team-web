a61e5f6c38ea514d6c3c8dbe7a915c4b9fa7c871
dongyusi <dongyusi@aixuedai.com>
2018-07-06 19:26:47 +0800

    * build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import Axios from 'axios'
+import Axios from 'axios'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/googlecode.css'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -36,7 +36,7 @@ Vue.prototype.$prompt = MessageBox.prompt
 Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
-// Axios.defaults.withCredentials = true
+Axios.defaults.withCredentials = true
 
 Vue.directive('highlight', (el) => {
     let blocks = el.querySelectorAll('pre code')
